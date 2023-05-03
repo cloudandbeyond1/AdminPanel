8b1f2bc34b03e8a3e89d0d12c73f57f0081bd8a1
Jayasri <jayadhanasvs@gmail.com>
2023-05-03 16:31:46 +0530

    add restaurent

--- src/jsx/components/Restaurant/AddRestaurant.js
+++ src/jsx/components/Restaurant/AddRestaurant.js
@@ -12,74 +12,7 @@ const AddRestaurant = () => {
                <div className="card">
                   <div className="card-body">
                      <div className="row">
-                        {/* <div className="col-md-4 order-md-2 mb-4">
-                           <h4 className="d-flex justify-content-between align-items-center mb-3">
-                              <span className="text-muted">Your cart</span>
-                              <span className="badge badge-primary badge-pill">
-                                 3
-                              </span>
-                           </h4>
-                           <ul className="list-group mb-3">
-                              <li className="list-group-item d-flex justify-content-between lh-condensed">
-                                 <div>
-                                    <h6 className="my-0">Product name</h6>
-                                    <small className="text-muted">
-                                       Brief description
-                                    </small>
-                                 </div>
-                                 <span className="text-muted">$12</span>
-                              </li>
-                              <li className="list-group-item d-flex justify-content-between lh-condensed">
-                                 <div>
-                                    <h6 className="my-0">Second product</h6>
-                                    <small className="text-muted">
-                                       Brief description
-                                    </small>
-                                 </div>
-                                 <span className="text-muted">$8</span>
-                              </li>
-                              <li className="list-group-item d-flex justify-content-between lh-condensed">
-                                 <div>
-                                    <h6 className="my-0">Third item</h6>
-                                    <small className="text-muted">
-                                       Brief description
-                                    </small>
-                                 </div>
-                                 <span className="text-muted">$5</span>
-                              </li>
-                              <li className="list-group-item d-flex justify-content-between active">
-                                 <div className="text-white">
-                                    <h6 className="my-0 text-white">
-                                       Promo code
-                                    </h6>
-                                    <small>EXAMPLECODE</small>
-                                 </div>
-                                 <span className="text-white">-$5</span>
-                              </li>
-                              <li className="list-group-item d-flex justify-content-between">
-                                 <span>Total (USD)</span>
-                                 <strong>$20</strong>
-                              </li>
-                           </ul>
-
-                           <form onSubmit={(e) => e.preventDefault()}>
-                              <div className="input-group">
-                                 <input
-                                    type="text"
-                                    className="form-control"
-                                    placeholder="Promo code"
-                                 />
-                                 <div className="input-group-append">
-                                    <button
-                                       type="submit"
-                                       className="btn btn-primary"
-                                    >
-                                       Redeem
-                                    </button>
-                                 </div>
-                              </div>
-                           </form>
-                        </div> */}
+                     
                         <div className="col-md-12 order-md-1">
                            <h4 className="mb-3"><i class="fa fa-cutlery"></i> Restaurant Information</h4>
                            <hr className="mb-4" />
